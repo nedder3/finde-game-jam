8b0d0af0042253ffe100b38dffe23a49261b8fa0
Jeremias Pellegrino <jeremiaspellegrino@gmail.com>
2023-04-20 20:15:28 -0300

    fix indentation

--- Assets/HighscoreScript.cs
+++ Assets/HighscoreScript.cs
@@ -8,82 +8,52 @@ public class HighscoreScript : MonoBehaviour
 {
     public Text textoPuntos;
     public Text textoHigscore;
+    private int puntos;
+    private int highscoreRecord;
 
-   private int puntos;
-   private int highscoreRecord;
-  
-
-
-     private void Awake() 
-     {
-            
-        LoadFromJson();
-
-
-     }
-
-   private void OnTriggerEnter2D(Collider2D other) 
+    private void Awake()
     {
-        
+        LoadFromJson();
+    }
 
-            //intento pescar pez
-
-        if(other.CompareTag("pez"))
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //intento pescar pez
+        if (other.CompareTag("pez"))
         {
             //si atrapo un pez sumo punto y lo muestro
-             puntos++;
-             textoPuntos.text=puntos.ToString();
-            
+            puntos++;
+            textoPuntos.text = puntos.ToString();
 
-            
-           
-
-                       
-            
             //actualizo highscore si los puntos superiores al highscore anterior
-            if(puntos>highscoreRecord)
+            if (puntos > highscoreRecord)
             {
-
-                 highscoreRecord = puntos;
-
-                 textoHigscore.text=highscoreRecord.ToString();
-                
-                
-                 SaveToJson();
-
+                highscoreRecord = puntos;
+                textoHigscore.text = highscoreRecord.ToString();
+                SaveToJson();
             }
         }
 
-        
+
     }
-    
+
     public void SaveToJson()
     {
-        
-
         HighscoreDATA data = new HighscoreDATA();
 
-        data.Highscore= highscoreRecord.ToString();
+        data.Highscore = highscoreRecord.ToString();
 
-        string json = JsonUtility.ToJson(data,true);
-        File.WriteAllText(Application.dataPath+"/HighscoreDATAfile.json",json);
-       
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(Application.dataPath + "/HighscoreDATAfile.json", json);
+
         Debug.Log("Entro a guardar JSON :");
     }
 
     public void LoadFromJson()
     {
-
-        string json = File.ReadAllText(Application.dataPath+"/HighscoreDATAfile.json");
+        string json = File.ReadAllText(Application.dataPath + "/HighscoreDATAfile.json");
         HighscoreDATA data = JsonUtility.FromJson<HighscoreDATA>(json);
 
-        Debug.Log( data.Highscore);
-       
-
-            
-
+        Debug.Log(data.Highscore);
     }
-
-
-    
 }
